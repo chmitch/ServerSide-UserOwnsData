72a3465961a26a29fff2db3ce468745cb2e2c5dd
Chris Mitchell (OCP) <chmitch@microsoft.com>
2018-06-07 11:10:45 -0400

    Initial checkin

---
+++ PowerBIEmbedded_SSUserOwnsData/Models/EmbedConfig.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.PowerBI.Api.V2.Models;
+using System;
+
+namespace PowerBIEmbedded_AppOwnsData.Models
+{
+    public class EmbedConfig
+    {
+        public string Id { get; set; }
+
+        public string EmbedUrl { get; set; }
+
+        public string EmbedToken { get; set; }
+        
+        
+        public bool? IsEffectiveIdentityRolesRequired { get; set; }
+
+        public bool? IsEffectiveIdentityRequired { get; set; }
+
+        public bool EnableRLS { get; set; }
+
+        public string Username { get; set; }
+
+        public string Roles { get; set; }
+
+        public string ErrorMessage { get; internal set; }
+    }
+}
